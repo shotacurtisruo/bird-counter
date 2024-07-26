74420bba071ce40616912076c143750556337d7e
Shota Ruo <159093194+shotacurtisruo@users.noreply.github.com>
2024-07-25 22:18:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count += 1
+    count  = count + 1
     countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - "
-    saveEl.textContent += countStr
+    saveEl.textContent = countStr + saveEl.textContent
     countEl.textContent = 0
     count = 0
 }
