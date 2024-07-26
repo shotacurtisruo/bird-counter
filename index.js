6592bc0cd418b81d43b3b930524cfad1755d2c1c
Shota Ruo <159093194+shotacurtisruo@users.noreply.github.com>
2024-07-26 13:12:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count  += 1
+    count += 1
     countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - "
-    saveEl.textContenttStr
+    saveEl.textContent += countStr  // Append the new count
     countEl.textContent = 0
     count = 0
 }
+
